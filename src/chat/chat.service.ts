edbcc908eb5b17ad55e1b01c1db838c4d9a4a215
aayushrathor <rathoreaayush84@gmail.com>
2023-05-02 11:36:24 +0530

    dialogflow adc auth

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,20 +1,36 @@
 import { SessionsClient } from '@google-cloud/dialogflow';
+import { Storage } from '@google-cloud/storage';
 import { Injectable } from '@nestjs/common';
-import { uuid } from 'uuidv4';
+import { v4 as uuid } from 'uuid';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 @Injectable()
 export class ChatService {
-    private SessionClient = new SessionsClient()
-    private projectId = process.env.GOOGLE_DIALOGFLOW_PROJECT_ID;
-    private location = "us-central1";
+    constructor() {
+        this.authenticateImplicitWithAdc();
+    }
+
+    private projectId = process.env.GOOGLE_DIALOGFLOW_PROJECT_ID || "my-project";
+    private location = process.env.GOOGLE_DIALOGFLOW_LOCATION || "global";
     private languageCode = process.env.GOOGLE_DIALOGFLOW_LANGUAGE_CODE || 'en-US';
+
+    private config = {
+        apiEndpoint: process.env.GOOGLE_DIALOGFLOW_API_ENDPOINT
+    }
+
+    private sessionClient = new SessionsClient({ apiEndpoint: this.config.apiEndpoint });
+
+    async authenticateImplicitWithAdc() {
+        const storage = new Storage({
+            projectId: this.projectId
+        });
+    }
 
     async detectIntentText(query: string) {
         const sessionID = uuid();
-        const sessionPath = this.SessionClient.projectLocationAgentSessionPath(
+        const sessionPath = this.sessionClient.projectLocationAgentSessionPath(
             this.projectId,
             this.location,
             sessionID
@@ -24,14 +40,14 @@ export class ChatService {
             session: sessionPath,
             queryInput: {
                 text: {
-                    text: query
+                    text: query,
+                    languageCode: this.languageCode
                 },
-                languageCode: this.languageCode
             }
         };
-
-        const [response] = await this.SessionClient.detectIntent(request);
-        const result = response[0].queryResult;
+        
+        const [response] = await this.sessionClient.detectIntent(request);
+        const result = response.queryResult;
 
         return {
             text: result.fulfillmentText,
