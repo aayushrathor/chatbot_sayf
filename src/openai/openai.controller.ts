e1ae3e19eed6fd46c092cd2ed6f695b3b4abbc83
aayushrathor <rathoreaayush84@gmail.com>
2023-05-02 18:10:31 +0530

    chatgpt integration

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { OpenaiService } from './openai.service';
 
 @Controller('openai')
-export class OpenaiController {}
+export class OpenaiController {
+    constructor(private readonly openaiService: OpenaiService) {}
+
+    @Post('completion')
+    async completion(@Body('prompt') prompt: string) : Promise<string> {
+        return await this.openaiService.completion(prompt);
+    }
+}
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
-export class OpenaiService {}
+export class OpenaiService {
+    private openaiurl = "https://api.openai.com/v1/chat/completions"
+
+    async completion(prompt: string): Promise<any> {
+        const response = await axios.post(this.openaiurl, {
+            "model": "gpt-3.5-turbo",
+            messages: [{ role: 'user', content: prompt }],
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            }
+        })
+
+        return {
+            "prompt": prompt,
+            "response": response.data["choices"][0]["message"]
+        }
+    }
+}
